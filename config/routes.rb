a69d1bf1435e7479ce3438385e62cf17e866ee7c
benjaminrdickson <bdickson8619@gmail.com>
2021-05-24 22:35:06 -0500

    adds chaining methods

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,10 @@ patch "/actors/:id", controller: "actors", action: "update"
 
 delete "/actors/:id", controller: "actors", action: "destroy"
 
+post "/users", controller: "users", action: "create"
+
+post "/sessions", controller: "sessions", action: "create"
+
 
 
 end
