61912234d0dd5a622a2d9258739a92353a67912c
benjaminrdickson <bdickson8619@gmail.com>
2021-05-26 09:27:56 -0500

    adds last nights hw

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-movie = Movie.new({title: "Training Day", year: 2001, plot: "A rookie cop spends his first day as a Los Angeles narcotics officer with a rogue detective who isn't what he appears to be."})
-movie.save
+# movie = Movie.new({title: "Training Day", year: 2001, plot: "A rookie cop spends his first day as a Los Angeles narcotics officer with a rogue detective who isn't what he appears to be."})
+# movie.save
 
-movie = Movie.new({title: "Tenet", year: 2020, plot: "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time."})
-movie.save
+# movie = Movie.new({title: "Tenet", year: 2020, plot: "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time."})
+# movie.save
 
-movie = Movie.new({title: "About Time", year: 2013, plot: "The film is about a young man with the ability to time travel who tries to change his past in hopes of improving his future."})
-movie.save
+# movie = Movie.new({title: "About Time", year: 2013, plot: "The film is about a young man with the ability to time travel who tries to change his past in hopes of improving his future."})
+# movie.save
+
+# genre = Genre.create!([
+#   {name: "Action"},
+#   {name: "Romantic-Comedy"},
+#   {name: "Thriller"},
+#   {name: "Science Fiction"}
+
+# ])
+
+movie_genre = MovieGenre.create!([
+  {genre_id: 1, movie_id: 13},
+  {genre_id: 2, movie_id: 3 },
+  {genre_id: 3, movie_id: 1 },
+  {genre_id: 4, movie_id: 2 }
+
+]) 
